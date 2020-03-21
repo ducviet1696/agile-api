48d089c3f7736bdb413d02c22112b50f016deb56
ducviet1696 <phamviet008@gmail.com>
2020-03-21 20:55:26 +0700

    removing issue

--- src/action.js
+++ src/action.js
@@ -4,8 +4,7 @@ export const action = (controller, action) => {
 
     container.autoSingleton(controller);
     return async (context, next) => {
-        console.log(action);
-        // const controllerInstance = container.make(controller);
+        const controllerInstance = container.make(controller);
         return await controllerInstance[ action ](context, next);
     };
 };
