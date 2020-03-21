5169ee912fddc482f2f2d2f67adbe7ada500b550
ducviet1696 <phamviet008@gmail.com>
2020-03-21 20:38:23 +0700

    adding auth controller

---
+++ src/apiV1/controllers/auth/AuthController.js
@@ -0,0 +1,32 @@
+import Controller                from '../Controller';
+import { Success, UnAuthorized } from '../../../services/http/message';
+import jsonwebtoken              from 'jsonwebtoken';
+
+export default class AuthController extends Controller {
+
+    constructor(auth, config) {
+        super();
+        this.authenticator = auth;
+        this.config        = config;
+    }
+
+    static get dependencies() {
+        return ['auth', 'config'];
+    }
+
+    async login(context) {
+
+        let credential;
+        const { jwt } = this.config;
+
+        try {
+            credential = await this.authenticator.byPassword(context.request.body.username, context.request.body.password);
+        } catch (e) {
+            return context.status = new UnAuthorized({});
+        }
+
+        context.body = new Success({
+            token: jsonwebtoken.sign(credential.toJSON(), jwt.privateKey, jwt.options),
+        });
+    }
+}
