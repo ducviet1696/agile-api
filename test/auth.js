94ad2b3b0afc51ca3ea819f4ed2000f06fdcda10
ducviet1696 <phamviet008@gmail.com>
2020-03-21 22:26:50 +0700

    fixing auth controller response

--- test/auth.js
+++ test/auth.js
@@ -1,4 +1,4 @@
-const chai   = require('chai');
+const chai     = require('chai');
 const chaiHttp = require('chai-http');
 const server   = require('../server');
 const should   = chai.should();
@@ -7,12 +7,8 @@ chai.use(chaiHttp);
 
 describe('auth', () => {
     beforeEach((done) => {
-        //Before each test we empty the database in your case
         done();
     });
-    /*
-     * Test the /GET route
-     */
     describe('/POST login', () => {
         it('login to agile', (done) => {
             let account = {
